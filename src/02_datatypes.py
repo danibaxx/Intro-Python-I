18d881d3400342ead445b3b11520318c7beb9e67
danibaxx <danibaxx@hotmail.com>
2020-06-03 18:37:11 -0600

    changed 01 structure, 02 complete

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,18 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+x = 5
+y = 7
+add = x + y
+print(add)
+# print(x + y) # maybe dont like?
 
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+x = "5"
+y = "7"
+combine = x + y
+print(combine)
+# print(x + y) # maybe dont like?
